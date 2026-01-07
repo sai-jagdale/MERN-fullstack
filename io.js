d00ee8474c4d610502efa8737d342dd693d7bcb3
Sai Jagdale <161121425+sai-jagdale@users.noreply.github.com>
2026-01-07 15:30:27 +0530

    Clean up io.js by removing unused code
    Removed commented-out JSON object and related console logs.

--- io.js
+++ io.js
@@ -30,36 +30,4 @@ var Students = [
 
 console.log(fruit);
 console.log(marks);
-=======
 
-//Json format Object
-// JSON --- JavaScript Object Notation
-var sai = {
-    name: "sai",
-    age: 21,
-    city: "Pune",
-    Class: "LY"
-}
-
-// Printing the values
-console.log(sai.name);
-console.log("Hello, World!")
-console.log(sai);
-
-// empty array
-var flower = [];
-// array of string 
-var fruit = ["Mango", "Apple", "Banana", "Grapes"];
-// array of numbers
-var marks = [90, 85, 88, 92];
-// array of json objects
-var Students = [
-    { name: "sai",age: 21,city: "Pune",Class: "LY"},
-    { name: "ram",age: 22,city: "Mumbai",Class: "TY"},
-    { name: "sham",age: 20,city: "Nashik",Class: "FY"}  
-];
-
-console.log(fruit);
-console.log(marks);
->>>>>>> 6ac402e7e8b75601b3a66ea90fdd7f0365b55284
-console.log(Students);
