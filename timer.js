05905d56b89ea2b6bb83948f914512d059084be9
Sai Jagdale <161121425+sai-jagdale@users.noreply.github.com>
2026-01-07 15:31:42 +0530

    Clean up timer.js by removing unused functions
    Removed unused functions and redundant code from timer.js.

--- timer.js
+++ timer.js
@@ -28,38 +28,3 @@ var number2 = 55;
 var total = addition(number1, number2);
 
 console.log("Total = " + total);
-
-
-=======
-// Javascript function 
-
-function show(){
-    var count = 67;
-    count++;
-    console.log(count);
-}
-
-function display(company){
-
-    console.log("Company name =" + company);
-}
-
-function addition(num1 , num2){
-    var result = num1 + num2;
-    console.log("Result = "+ result);
-}
-
-show(); // invoking javascript function
-
-var provider = "Transflower";
-display(provider);
-addition(10,20);
-
-var number1 = 45;
-var number2 = 55;
-var total = addition(number1, number2);
-
-console.log("Total = " + total);
-
-
->>>>>>> 6ac402e7e8b75601b3a66ea90fdd7f0365b55284
