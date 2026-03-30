8572aceb2829710f78318fe4bb7ffb8c8f4babd4
sai.jagdale <161121425+sai-jagdale@users.noreply.github.com>
2026-03-30 17:11:45 +0530

    React Practice

---
+++ reactappdemo/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="\images\GirlBeforeAMirror.jpg" className="App-logo" alt="logo" />
+        <p> Doing ordinary things extra ordinarily </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn us learn Dynamic UI Programming with React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
