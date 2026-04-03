2aed2a6356ec5eca014a31198e84f1adb38084de
sai.jagdale <161121425+sai-jagdale@users.noreply.github.com>
2026-04-03 10:59:38 +0530

    Event binding in React

---
+++ reactappdemo/src/Counter.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+class Counter extends React.Component{
+
+    // states are introduced since props are 
+    // read-only and cannot be changed
+    // so the value of props is stored 
+    // in stated and then changed using setState method
+    constructor(props){
+        super(props);
+        this.state={ count:props.count }
+    }
+
+    render(){
+        return(
+            <div>
+                <button onClick={()=>{
+                    //this.props.count=this.props.count-1;
+                    //console.log("dislike");
+                    
+                    this.setState({count:this.state.count-1});
+                    
+                }}>-</button>
+
+                {/* here the value comens from the likes 
+                value that is associated with the product */}
+                <input value={this.state.count} type="text"/>
+                
+                {/* Properties(props) are ready only 
+                they cannot be changed */}
+
+                <button onClick={()=>{
+                    //this.props.count=this.props.count+1;
+                    //console.log("like");
+                    
+                    this.setState({count:this.state.count+1});
+
+                }}>+</button>
+            </div>
+        )
+    }
+}
+
+export default Counter;
--- reactappdemo/src/Product.js
+++ reactappdemo/src/Product.js
@@ -1,6 +1,33 @@
 import React from 'react';
+import Counter from './Counter';
 
 class Product extends React.Component {
+
+    constructor(props){
+        super(props);
+        //initializing state by taking value from property
+        this.state = {likes:props.likes};
+        // Event binding in react is done in the constructor 
+        // of the class component.
+        // this handler is the callback function that 
+        // is passed to the counter component.
+        this.handler = this.handler.bind(this);
+    }
+
+
+    // Callback function 
+    handler(data){
+        // here data is the value that is passed from 
+        // the counter component and it is used to 
+        // update the state of the product component
+        this.setState({likes:data});
+
+        // this changes the valu of likes in the product component 
+        // and then the updated value is passed to the counter 
+        // component as a property and then the counter component 
+        // re-renders with the updated value of likes
+    }
+
     render(){
         return(
             <div>
@@ -15,6 +42,9 @@ class Product extends React.Component {
                     <p>Price : Rs{this.props.price}</p>
                     <p>Color : {this.props.color}</p>
                     <p>Likes : {this.props.likes}</p>
+                    {/* here count is the property of Counter , that is passed to 
+                    the counter component */}
+                    <Counter count={this.props.likes} handler={this.handler}></Counter>
                 <br/>
                 <button>Add to Cart</button>
 
